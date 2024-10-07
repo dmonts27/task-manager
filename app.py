4ca82d9fe080ba14002a14286e78ec62e586acba
David Montes <davidmontes2727@gmail.com>
2024-10-06 22:19:37 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, request, redirect, url_for
+import mysql.connector
+
+app = Flask(__name__)
+
+# MySQL Connection Config
+def connect_db():
+    conn = mysql.connector.connect(
+        host="localhost",
+        user="root",  
+        password="Wabbit10Interstellar",  
+        database="task_manager_db"  
+    )
+    return conn
+
+# Home route to display tasks
+@app.route('/')
+def index():
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('SELECT * FROM tasks')
+    tasks = cursor.fetchall()  # Fetch all tasks from the database
+    conn.close()
+    return render_template('index.html', tasks=tasks)
+
+# Add new task route
+@app.route('/add', methods=['POST'])
+def add_task():
+    task_content = request.form['task_content']  # Get task content from the form
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('INSERT INTO tasks (content) VALUES (%s)', (task_content,))
+    conn.commit()  # Commit the transaction
+    conn.close()
+    return redirect(url_for('index'))
+
+# Delete task route
+@app.route('/delete/<int:id>')
+def delete_task(id):
+    conn = connect_db()
+    cursor = conn.cursor()
+    cursor.execute('DELETE FROM tasks WHERE id=%s', (id,))
+    conn.commit()
+    conn.close()
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
